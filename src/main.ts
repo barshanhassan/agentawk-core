7526f24a6d25035edb92c9f2914491561578f1f3
talhashahh <talhabkh@gmail.com>
2026-05-07 10:51:40 +0500

    feat: initialize NestJS application with global prefix, CORS configuration, and BigInt serialization support

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://ezconn-fe.web.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
