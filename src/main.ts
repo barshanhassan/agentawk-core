a4728dfc23d61ea49b656bb0d727bb175303bcee
talhashahh <talhabkh@gmail.com>
2026-05-07 10:48:40 +0500

    feat: initialize NestJS application with global prefix, CORS configuration, and BigInt serialization support

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.enableCors({
-    origin: true,
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3001);
 }
 // Triggering rebuild for Gallery local storage changes
